a35d45390e4646d78c25408ccb3cf67e963ff7b6
dkzks44 <dkzks44@naver.com>
2021-11-02 04:34:52 +0900

    time setting
    set current time

--- jg_scrap.py
+++ jg_scrap.py
@@ -1,14 +1,18 @@
 from bs4 import BeautifulSoup as BS
 import requests as req
 import json
+import time
 
 # data_1 = req.get("https://search-api.joongna.com/v25/search/product")
+now = time.localtime()
+current_time = time.strftime('%Y-%m-%d %X')
+
 data_jg = req.post("https://search-api.joongna.com/v25/search/product", json={
     "filter": {"categoryDepth": 0, "categorySeq": 0,
                "dateFilterParameter": {"sortEndDate": None, "sortStartDate": None}, "flawedYn": 0,
                "fullPackageYn": 0, "limitedEditionYn": 0, "maxPrice": 2000000000, "minPrice": 0,
-               "productCondition": -1, "tradeType": 0}, "osType": 2, "page": 1, "searchQuantity": 3,
-    "searchStartTime": "2021-11-01 16:21:03", "searchWord": "아이패드", "sort": "RECOMMEND_SORT", "startIndex": 0,
+               "productCondition": -1, "tradeType": 0}, "osType": 2, "page": 1, "searchQuantity": 1,
+    "searchStartTime": current_time, "searchWord": "아이패드", "sort": "RECOMMEND_SORT", "startIndex": 0,
     "productFilter": "APP"})
 soup_jg = BS(data_jg.text, 'html.parser')
 a = soup_jg.text
@@ -26,5 +30,5 @@ product_data = json.loads(a)
 #     jg_product_time = product_data["data"]["items"][i]["articleRegDate"]
 # print(jg_product_image, jg_product_title, jg_product_price, jg_product_location, jg_product_time)
 
-# jg_product_image = product_data["data"]["items"][1]["detailImgUrl"]
-# print(jg_product_image)
+jg_product_image = product_data["data"]["items"][0]["detailImgUrl"]
+print(jg_product_image)
